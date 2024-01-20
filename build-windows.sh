ff241bb8f1ff6a69f5a80762ba0f5c282e300332
Lachlan O'Dea <lodea@mac.com>
2024-01-20 22:42:23 +1100

    try windows again

--- build-windows.sh
+++ build-windows.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+repo_dir=$(pwd)
+
 cd libuv-build
 
 mkdir -p build
@@ -9,6 +11,6 @@ cmake --build build
 
 find build
 
-echo SBT_NATIVE_COMPILE='set nativeCompileOptions += "--include-directory=libuv-build/include" ; ' >> $GITHUB_ENV
+echo SBT_NATIVE_COMPILE="set nativeCompileOptions += \"--include-directory=$repo_dir/libuv-build/include\" ; " >> $GITHUB_ENV
 
-echo SBT_NATIVE_LINK='set nativeLinkingOptions += "--library-directory=libuv-build/build/Debug" ; ' >> $GITHUB_ENV
+echo SBT_NATIVE_LINK="set nativeLinkingOptions += \"--library-directory=$repo_dir/libuv-build/build/Debug\" ; " >> $GITHUB_ENV
